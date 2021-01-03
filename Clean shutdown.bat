7d46c2e6ae5c7650e5adfc001ffc9712574cbd51
Rahul Kumar <rahul4michan@gmail.com>
2021-01-03 21:45:24 +0530

    simple script to clean temp file and shutdown pc

---
+++ Clean shutdown.bat
@@ -0,0 +1,12 @@
+@echo off
+Rem change the extension to .bat
+Rem Auhor Rahul Kumar
+Rem Date 03 Jan 2021
+Rem supported OS Windows 10
+echo "Please provide elevated permission to clean temp files..."
+echo "loading please wait...."
+if not "%1"=="am_admin" (powershell start -verb runas '%0' am_admin & exit /b)
+del "%tmp%\*.*" /s /q /f
+FOR /d %%p IN ("%tmp%\*.*") DO rmdir "%%p" /s /q
+Rem shutdown -s -t 0
+pause
